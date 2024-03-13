1b384890e25fe293bf4d0d2bf70f871aa51acb8c
jamseyboy <buongahrangchal@gmail.com>
2024-03-14 02:47:57 +0530

    user ID DTO added

---
+++ src/main/java/com/jameslalringsan/pune_hmar/dto/usrIdDto.java
@@ -0,0 +1,20 @@
+package com.jameslalringsan.pune_hmar.dto;
+
+public class usrIdDto {
+
+
+    public  Integer retrieveduserID;
+    public usrIdDto(Integer retrieveduserID) {
+        this.retrieveduserID = retrieveduserID;
+    }
+
+    public Integer getRetrieveduserID() {
+        return retrieveduserID;
+    }
+
+    public void setRetrieveduserID(Integer retrieveduserID) {
+        this.retrieveduserID = retrieveduserID;
+    }
+
+
+}
