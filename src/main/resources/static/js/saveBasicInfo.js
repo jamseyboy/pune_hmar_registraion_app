73b35abb3296e46dd13e8dcda4e44d3e154730df
jamseyboy <buongahrangchal@gmail.com>
2024-03-16 03:46:39 +0530

    added save user basic info functionality in front end

---
+++ src/main/resources/static/js/saveBasicInfo.js
@@ -0,0 +1,50 @@
+var app = angular.module("SaveUserDetailsApp",[]);
+
+app.config(['$qProvider', function ($qProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+}]);
+
+
+app.controller("submitUserController", function($scope,$http, $location, $window){
+
+    $scope.basicInfo ={};
+    $scope.isVisible = false;
+
+    $scope.submitUser = function(){
+
+        console.log($scope.basicInfo);
+        $scope.isVisible = true;
+    }
+
+    $scope.nextPage = function(){
+
+        $scope.basicInfo.dob = formatDate($scope.basicInfo.dob);
+        console.log($scope.basicInfo);
+
+        $http({
+
+            method: 'POST',
+            url: 'http://localhost:8080/hsa/users/saveUser',
+            data: $scope.basicInfo
+        }).then(function saveUser(response){
+
+            localStorage.setItem('userId', JSON.stringify(response.data))
+    
+            $window.location.href = '/allUserDetails';
+        });
+        
+
+    
+        
+    }
+
+    
+
+    function formatDate(date) {
+        var yyyy = date.getFullYear().toString();
+        var mm = (date.getMonth() + 1).toString().padStart(2, '0');
+        var dd = date.getDate().toString().padStart(2, '0');
+        return yyyy + '-' + mm + '-' + dd;
+    }
+
+});
