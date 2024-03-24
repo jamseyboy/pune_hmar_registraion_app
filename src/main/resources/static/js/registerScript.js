8cb015d6856944aecb6634ac2617f4f91298ec9d
jamseyboy <buongahrangchal@gmail.com>
2024-03-25 02:54:51 +0530

    implemented status in register service and implemented loading animation in UI

---
+++ src/main/resources/static/js/registerScript.js
@@ -0,0 +1,72 @@
+var app = angular.module("registerApp", []);
+
+app.config(['$qProvider', function ($qProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+}]);
+
+
+
+app.controller("registerController", function($scope, $window, $http,$timeout){
+    $scope.waitMessage="Loading...."
+    $scope.isVisible=false;
+    $scope.loading = false;
+    
+
+    $scope.register = function(){
+            if($scope.user && $scope.user.password == $scope.user.confirmPassword){
+                $scope.isVisible = false;
+                $scope.loading =true;
+                    $scope.userData = {
+                        email : $scope.user.email,
+                        password : $scope.user.password,
+                        status : $scope.user.status
+                        }
+                        $timeout(function(){
+                            $scope.waitMessage="Processing...."
+                            $http({
+                                method: 'POST',
+                                url: "http://localhost:8080/hsa/users/registerUser",
+                                data: $scope.userData
+                            }).then(function(response){
+    
+                                if(response.data != null && response.data == "1"){
+                                    $scope.waitMessage="Success"
+                                    $window.location.href="/basicInfo.html"
+                                }
+                                else if(response.data != null && response.data == "0"){
+                                    $scope.loading =false;
+                                    $scope.isVisible = true;
+                                    console.log(response.data)
+                                    $scope.alertMessage = "email id " + $scope.userData.email + " already exist try another one";
+                                }
+                                else{
+                                    $scope.loading=false;
+                                    $scope.isVisible = true;
+                                    console.log(error.data)
+                                    $scope.alertMessage = "An Error has occurred";
+                                }
+    
+                            }).catch(function(error){
+                                $scope.loading=false;
+                                $scope.isVisible = true;
+                                console.log(error.data)
+                                $scope.alertMessage = "An Error has occurred";
+                            })
+
+                        },2000);
+
+                        
+                    console.log($scope.userData);
+            }else if($scope.user && $scope.user.password != $scope.user.confirmPassword){
+                    $scope.isVisible = true;
+                    $scope.alertMessage = "Password does not Match !!!";
+                    console.log($scope.user);
+            }else{
+                    $scope.isVisible = true;
+                    $scope.alertMessage = "Something is wrong Contact admin !!!";
+            }
+            }
+
+
+
+});
